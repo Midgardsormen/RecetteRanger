b07bb12def95b0751a310581e1a2d6aab008fcb1
Maxime Decourtray <mdecourtray11@gmail.com>
2025-11-29 23:29:17 +0100

    fix: autocomplet letters with accent

--- src/view/components/ui/ListItem/ListItem.svelte
+++ src/view/components/ui/ListItem/ListItem.svelte
@@ -228,7 +228,7 @@
     display: flex;
     align-items: center;
     justify-content: center;
-    padding: $spacing-sm;
+    
 
     @media (min-width: $breakpoint-sm) {
       max-width: $list-item-thumbnail-max-width;
