2c03a9590c114e86744a53a309d2512e34348d3c
Maxime Decourtray <mdecourtray11@gmail.com>
2025-12-09 22:59:00 +0100

    fix:update4

--- src/back/modules/admin/admin.module.ts
+++ src/back/modules/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { SvelteRenderService } from '../../services/svelte-render.service';
 import { PrismaModule } from '../../shared/prisma/prisma.module';
+import { MealPlanModule } from '../../api/meal-plan/meal-plan.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MealPlanModule],
   controllers: [AdminController],
   providers: [SvelteRenderService],
 })
